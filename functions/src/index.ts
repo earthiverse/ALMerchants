c3d78ab76cdb5b1c6144e6f6c4091d40091989f3
Kent Rasmussen <hyprkookeez@gmail.com>
2021-09-04 12:51:51 +0900

    Add history tracking for prices

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as functions from "firebase-functions";
 import {SecretManagerServiceClient} from "@google-cloud/secret-manager"
 
 import * as AL from "alclient"
+import { ItemDataTrade, ItemName, TradeSlotType } from "alclient";
 
 // Log console logs
 require("firebase-functions/lib/logger/compat");
@@ -27,14 +28,100 @@ export const updateMerchants = functions.https.onRequest(async (req, res) => {
   // Get the latest merchant data
   await AL.Game.login(data.email, data.password, data.mongo)
   const merchantData = await AL.Game.getMerchants()
+  const now = Date.now()
+  const today = now - now % (60*60*24)
   res.send({data: merchantData})
 
-  // Update data in database
+  // Update merchant data
   for(const merchant of merchantData) {
     admin.database().ref(`merchants/${merchant.name}`).set(
       {
-        lastSeen: Date.now(),
+        lastSeen: now,
          ...merchant
       })
+  }
+
+  // Update price history data
+  const priceHistoryData: {
+    [T in ItemName]?: {
+      buying?: ItemDataTrade
+      selling?: ItemDataTrade
+    }
+  } = {  }
+
+  for(const merchant of merchantData) {
+    for(const slot in merchant.slots) {
+      const itemData = merchant.slots[slot as TradeSlotType]
+      if(itemData === undefined) continue // No item. This shouldn't happen, though.
+
+      const itemName = itemData.name
+
+      // TODO: I need a way to store price history for various levels, instead of just level 0
+      if(itemData.level !== undefined && itemData.level > 0) continue
+      // TODO: I need a way to store price history for various modifiers
+      if(itemData.p !== undefined) continue
+      // TODO: I need a way to store price history for various cosmetics
+      if(itemData.data) continue
+
+      if(priceHistoryData[itemName] === undefined) {
+        priceHistoryData[itemName] = { }
+      }
+      const newPriceHistoryData = priceHistoryData[itemName]
+      if(newPriceHistoryData === undefined) continue // NOTE: I know it's defined, but Typescript was giving me a lot of errors without this
+
+      if(itemData.b) {
+        // They are buying, check if it's a new high price
+        if(!newPriceHistoryData.buying) {
+          // We didn't have any data before
+          newPriceHistoryData.buying = itemData
+        } else {
+          // We have to check if they're paying more than the last offer
+          if(newPriceHistoryData.buying.price > itemData.price) continue // This offer isn't as good
+          newPriceHistoryData.buying = itemData
+        }
+      } else {
+        // They are selling, check if it's a new low price
+        if(!newPriceHistoryData.selling) {
+          // We didn't have any data before
+          newPriceHistoryData.selling = itemData
+        } else {
+          // We have to check if they're selling it for less than the last offer
+          if(newPriceHistoryData.selling.price < itemData.price) continue // This offer isn't as good
+          newPriceHistoryData.selling = itemData
+        }
+      }
+    }
+  }
+
+  for(const hName in priceHistoryData) {
+    const itemName = hName as ItemName
+    const newPriceHistoryData = priceHistoryData[itemName]
+    if(newPriceHistoryData === undefined) continue // NOTE: I know it's defined, but Typescript was giving me a lot of errors without this
+    
+    admin.database().ref(`history/${itemName}/${today}`).once("value", (data) => {
+      const oldPriceHistoryData = data.val()
+
+      if(oldPriceHistoryData == null) {
+        // We don't have old data, let's add some
+        admin.database().ref(`history/${itemName}/${today}`).set(newPriceHistoryData).catch((e) => console.error(e))
+        return
+      }
+
+      if(oldPriceHistoryData.buying == undefined && newPriceHistoryData.buying) {
+        // We don't have buying data, let's add some
+        admin.database().ref(`history/${itemName}/${today}/buying`).set(newPriceHistoryData.buying).catch((e) => console.error(e))
+      } else if(oldPriceHistoryData.buying && newPriceHistoryData.buying && newPriceHistoryData.buying.price > oldPriceHistoryData.buying.price) {
+        // Someone is paying more for the item now
+        admin.database().ref(`history/${itemName}/${today}/buying`).set(newPriceHistoryData.buying).catch((e) => console.error(e))
+      }
+
+      if(oldPriceHistoryData.selling == undefined && newPriceHistoryData.selling) {
+        // We don't have selling data, let's add some
+        admin.database().ref(`history/${itemName}/${today}/selling`).set(newPriceHistoryData.selling).catch((e) => console.error(e))
+      } else if(oldPriceHistoryData.selling && newPriceHistoryData.selling && newPriceHistoryData.selling.price < oldPriceHistoryData.selling.price) {
+        // Someone is selling the same item for less now
+        admin.database().ref(`history/${itemName}/${today}/selling`).set(newPriceHistoryData.selling).catch((e) => console.error(e))
+      }
+    })
   }
 });
